e091b81eee2a4c4bdc1b4bbe00b35cbbb26b16f6
XR Development <xr-dev@ll202.uni.lux>
2024-10-18 15:23:31 +0200

    include lidar point in merged map

--- merge_point_clouds/merge_point_clouds.py
+++ merge_point_clouds/merge_point_clouds.py
@@ -66,7 +66,7 @@ class PointCloudMerger(Node):
         msg = PointCloud2()
         self.merged_pc_pub.publish(msg)
 
-    def read_pc2_points(self, point_cloud, type:str):
+    def read_pc2_points(self, point_cloud:PointCloud2, type:str):
 
         if type == "lidar":
             return pc2.read_points_numpy(point_cloud, skip_nans=True, field_names=("x", "y", "z"))
@@ -74,10 +74,10 @@ class PointCloudMerger(Node):
         elif type == "rgb":
             return pc2.read_points_numpy(point_cloud, skip_nans=True, field_names=("x", "y", "z", "rgb"))
         else: 
-            print("Invalid input type")
+            self.get_logger().error("Invalid input type")
             return -1
 
-    def create_pointcloud2(self, points):
+    def create_pointcloud2(self, points:list):
 
         header = self.lidar_pc.header
         fields = [
@@ -90,7 +90,7 @@ class PointCloudMerger(Node):
 
         return pointcloud
     
-    def merge_point_clouds(self, lidar_pc, rgbd_pc):
+    def merge_point_clouds(self, lidar_pc:PointCloud2, rgbd_pc:PointCloud2):
         
         # Check that LiDAR pc and RGB-D pc are not empty
         if lidar_pc is None or rgbd_pc is None:
@@ -100,12 +100,14 @@ class PointCloudMerger(Node):
         lidar_points = self.read_pc2_points(lidar_pc, "lidar")
         rgbd_points = self.read_pc2_points(rgbd_pc, "rgb")
 
+
+
         rgb_tree = KDTree(rgbd_points[:,:3])
 
         merged_points = []
 
         for lidar_p in lidar_points:
-            distance, idx = rgb_tree.query(lidar_p[:3], k=1)
+            distance, idx = rgb_tree.query(lidar_p[:3], k=1) 
 
             if (distance < 0.05):
                 nearest_rgb_point = rgbd_points[idx]
@@ -113,10 +115,10 @@ class PointCloudMerger(Node):
                 rgb_value = nearest_rgb_point[3]
 
                 merged_points.append((nearest_rgb_point[0], nearest_rgb_point[1], nearest_rgb_point[2], rgb_value))
+                merged_points.append((lidar_p[0], lidar_p[1], lidar_p[2], rgb_value))
             else:
                 merged_points.append((*lidar_p,0))
 
-        
         merged_cloud = self.create_pointcloud2(merged_points)
 
         self.merged_pc_pub.publish(merged_cloud)
